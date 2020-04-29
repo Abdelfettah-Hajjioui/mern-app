ee697b549141609b2da18d87c5fd714b6966fe99
Abdelfettah-Hajjioui <50712908+Abdelfettah-Hajjioui@users.noreply.github.com>
2020-04-29 12:52:15 +0000

    mern update
    mern update

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 classNames='x-large'>DH Attendance system</h1>
+          <p className='lead'>
+            HD attendance system is a project of recognizing the students face
+            for taking attendance by using face biometrics based on high -
+            definition monitor video and other information technology. HD
+            attendance system is a project created by two students of ENSAH,
+            Omar DAKKAK and Abdelfettah HAJJIOUI, and supervised by professor
+            Mohammde ADDAM
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
